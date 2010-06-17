c263c69fcb7768b39f915b879060b3fd31a7e3e5
ymazuz <ymazuz@mac.com>
2010-06-17 02:59:13 -0400

    Added contextual menu functionality: If menu is evoked by right-click/ctrl-click on a link, the option is "Open Link in Papers", otherwise "Open Current Page in Papers".
    Shifted some variable names around because I'm weird like that.
    $	renamed:    OpenInPapers.safariextension/inject.js -> openInPapers.safariextension/inject.js
    	renamed:    OpenInPapers.safariextension/global.html -> openInPapers.safariextension/openInPapers.html

---
+++ openInPapers.safariextension/global.js
@@ -0,0 +1,108 @@
+// Register for the validate and command events.
+safari.application.addEventListener("validate", performValidate, false);
+safari.application.addEventListener("command", performCommand, false);
+safari.application.addEventListener("message", handleMessage, false);
+
+var ctrlStatusGlobal = false;
+
+// In the global page, the only events to listen for 
+// are toolbar or context menu events. This determines which it was.
+function performCommand(event)
+{	
+	thisTab = safari.application.activeBrowserWindow.activeTab;
+	theSelection = "";
+	
+	
+	if (event.command == "openLinkInPapers") 
+	// from the "Open Link in Papers" context menu event...
+	{
+		theUrl = event.userInfo.href; // pass the href as the URL
+		theTitle = event.userInfo.selection; // and the link text as the title
+	} 
+	else // otherwise it's "open in Papers" from toolbar or context menu
+	{
+		// so url and title come from the current page
+		theUrl = thisTab.url;
+		theTitle = thisTab.title;
+		
+		// if it was the context menu, we can get the selection
+		// TO-DO (ysm): figure out if we can get a selection 
+		// at the time of toolbar click
+		if (event.userInfo != undefined) 
+		{
+			theSelection = event.userInfo.selection;	
+		}	
+	}			
+	
+	// send the url, title, and selection to the open in function
+	openURLinPapers(theUrl, theTitle, theSelection);
+}
+
+
+function openURLinPapers(url, title, selection)
+{
+	papersUrl = 'papers://url/'+encodeURIComponent(url)+'&title='+encodeURIComponent(title);
+	
+	// if selection is not empty, append it, too
+	if (selection) 
+	{	
+		papersUrl = papersUrl + '&selectedText=' + encodeURIComponent(selection);		
+	} 	
+	// (ysm: I'm not sure "&selectedText=" has any effect. 
+	// It's in the Papers bookmarklet, though.)
+	
+	// The end of the line: open the "papers://" url
+	safari.application.activeBrowserWindow.activeTab.url = papersUrl;
+}
+
+
+function handleMessage(event)
+{
+	// TO-DO (ysm): figure out how to get selected text into here 
+	// (and if it actually matters to Papers)
+	console.log(event.name);
+    console.log(event.message);
+    
+    if (event.name == 'openInPapersViaKeyboardShortcut')
+    {
+        if (ctrlStatusGlobal == true)
+        {
+            activeTab = safari.application.activeBrowserWindow.activeTab;
+            openURLinPapers(activeTab.url, activeTab.title, "");
+        }
+    }
+    else if(event.name == 'ctrlStatusUpdate')
+    {
+        ctrlStatusGlobal = event.message;
+    }
+}
+
+
+
+
+
+	
+function performValidate(event)
+ {	
+	if (!safari.application.activeBrowserWindow.activeTab.url) {
+		event.target.disabled = true;
+	} else {
+		switch (event.command) {
+ 			case "openInPapersToolbar":
+ 				// as long as there's a URL, the toolbar item is enabled
+ 				event.target.disabled = false;
+ 				break;
+ 				
+ 			// the other events we can validate are context menu ones	
+ 			case "openInPapersMenu":
+ 				// the regular "Open in Papers" is available unless the user right-clicked a link
+ 				event.target.disabled = event.userInfo.hasLink;
+				break;
+				
+			case "openLinkInPapers":
+				// "Open Link in Papers" only if they did right-click a link
+				event.target.disabled = !event.userInfo.hasLink; 
+ 		} 
+ 	}
+ 	
+}
--- openInPapers.safariextension/inject.js
+++ openInPapers.safariextension/inject.js
@@ -21,7 +21,14 @@ function checkKeyDown(event)
     }
 }
 
-
+function checkKeyUp(event)
+{
+    // <ctrl> key release
+	if(event.keyCode == 17)
+	{
+		safari.self.tab.dispatchMessage('ctrlStatusUpdate', false);
+	}
+}
 
 function getContextMenuEvent(event) {
 	
@@ -32,13 +39,15 @@ function getContextMenuEvent(event) {
 	// we can just add a userInfo object to it. 
 	var userInfo = new Object();
 
-
- 	userInfo.hasSelection = (sel.length != 0);   
-	userInfo.selection = userInfo.hasSelection ? selection : "";
-
-	// does the right-click target have an HREF attribute?
-	userInfo.hasLink = !(typeof event.target.href == "undefined");
-	userInfo.href = userInfo.hasLink ? event.target.href : null;
+	// the userInfo object's properties:
+	// hasLink: does the right-click target have an HREF attribute?
+	userInfo.hasLink = !(typeof event.target.href == "undefined"); 
+	
+	// selection: the selected text or the link text, depending on click target
+	userInfo.selection =  userInfo.hasLink ? event.target.innerText : selection;
+	
+	// href: the link URL if there is one
+	userInfo.href = userInfo.hasLink ? event.target.href : "";
     
 	// add userInfo to the event so it'll be seen by the validate function 
 	safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
